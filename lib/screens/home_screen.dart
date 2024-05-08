ab3ad54d597a663c899d7a7fbfc93de38aff12c7
Miftah <miftahrahi24@gmail.com>
2024-05-08 18:37:08 +0600

    Date Time & Minimal Ux Update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -19,57 +20,160 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Drink Water Tracker'),
+        title: const Text('Water Consume Tracker'),
+        centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('Tap here to track water consume'),
-            const SizedBox(height: 10),
+
             GestureDetector(
               onTap: _addWaterConsume,
               child: Container(
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
+                  borderRadius: BorderRadius.circular(100),
                   border: Border.all(color: Colors.yellow, width: 8),
                 ),
                 child: const Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Icon(Icons.water_drop_outlined),
+                  padding: EdgeInsets.all(30),
+                  child: Column(
+                    children: [
+                      Icon(Icons.water_drop_outlined),
+                      Text('Tap Here',
+                        style: TextStyle(
+                          fontSize: 14
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
-            const SizedBox(height: 10),
+            const SizedBox(height: 20),
             SizedBox(
-              width: 80,
+              width: 100,
               child: TextField(
                 controller: _glassCountTEController,
                 keyboardType: TextInputType.number,
                 textAlign: TextAlign.center,
                 decoration: const InputDecoration(
+                  label: Text('Glass Number'),
                     contentPadding: EdgeInsets.all(5),
-                    border: OutlineInputBorder(),
-                    focusedBorder: OutlineInputBorder()
+                    border: OutlineInputBorder( borderSide: BorderSide(color: Colors.green)),
+                    focusedBorder: OutlineInputBorder( borderSide: BorderSide(color: Colors.green)),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green)
+                  )
                 ),
               ),
             ),
-            SizedBox(height: 10),
-            Text('History'),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
+            const Text('Type of Glass',
+              style: TextStyle(
+                  fontSize: 18
+              ),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: 90,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(
+                      color: Colors.yellowAccent,
+                      width: 4
+                    )
+                  ),
+                  child: const Center(
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Icon(Icons.coffee_outlined),
+                          Text('250 ML')
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 80,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                          color: Colors.yellowAccent,
+                          width: 4
+                      )
+                  ),
+                  child: const Center(
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Icon(Icons.coffee_outlined),
+                          Text('350 ML')
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 80,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                          color: Colors.yellowAccent,
+                          width: 4
+                      )
+                  ),
+                  child: const Center(
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Icon(Icons.coffee_outlined),
+                          Text('450 ML')
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('History',
+                style: TextStyle(
+                  fontSize: 18
+                ),
+                ),
+                Text('Total: ${_getTolatWaterConsumeCount()}',
+                  style: const TextStyle(
+                      fontSize: 18
+                  ),
+                )
+              ],
+            ),
+            const Divider(height: 10),
             Expanded(
               child: ListView.builder(
                   itemCount: waterConsumeList.length,
                   itemBuilder: (context, index) {
+                    var reversedList = waterConsumeList.reversed.toList();
                     return ListTile(
-                      title: Text(waterConsumeList[index].time.toString()),
+                      title: Text(DateFormat.yMEd().add_jm().format(waterConsumeList[index].time)),
                       leading: CircleAvatar(
                         child: Text('${index + 1}'),
                       ),
                       trailing: Text(
                         waterConsumeList[index].glassCount.toString(),
-                        style: TextStyle(fontSize: 18,
+                        style: const TextStyle(fontSize: 18,
                             fontWeight: FontWeight.bold
                         ),
                       ),
